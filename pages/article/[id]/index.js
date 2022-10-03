e646d7ba6ace30cc8d01850c3f27a7f24c3468be
jesjo001 <automationlounge@gmail.com>
2022-10-03 11:26:11 +0100

    meta section implementation

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,4 +1,8 @@
 import { useRouter } from "next/router"
+import Meta from "../../../components/Meta"
+import { server } from "../../../config"
+
+
 
 const article = ({article}) => {
     // const router = useRouter()
@@ -6,6 +10,7 @@ const article = ({article}) => {
 
   return (
     <>
+    <Meta title={article.title} description={article.excerpt} />
     <h1>{ article.title }</h1>
     <p>{article.body}</p>
     </>
@@ -14,7 +19,7 @@ const article = ({article}) => {
 
 export const getStaticProps = async (context) => {
 
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
     const article = await res.json()
 
@@ -26,7 +31,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles`)
 
     const articles = await res.json()
     const ids = articles.map(article => article.id)
@@ -37,6 +42,32 @@ export const getStaticPaths = async () => {
         fallback: false,
     }
 }
+
+// export const getStaticProps = async (context) => {
+
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+//     const article = await res.json()
+
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+//     const articles = await res.json()
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+
+//     return {
+//         paths,
+//         fallback: false,
+//     }
+// }
 
 export default article
 
--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ArticleList from '../components/ArticleList'
+import {server} from '../config'
 
 export default function Home({articles}) {
   console.log(articles)
   return (
     <div className={styles.container}>
-        <Head>
-          <title>AutomationLounge News</title>
-          <meta name="description" content="AutomationLounge" />
-        </Head>
-
         <ArticleList articles={articles} />
     </div>
   )
 }
 
-import {server} from '../config'
 export const getStaticProps = async () => {
   const res = await fetch(`${server}/api/articles`)
 
@@ -29,3 +24,15 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`${server}/api/articles`)
+
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
